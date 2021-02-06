500f99d6eca090db0ad9c210740bcbfe19bda722
zaemon1251 <heste.m@icloud.com>
2021-02-06 11:52:47 +0900

    make  a testcase

--- django/tutorial/kakeibo/forms.py
+++ django/tutorial/kakeibo/forms.py
@@ -2,9 +2,18 @@ from django import forms
 from .models import Kakeibo
 
 class KakeiboForm(forms.ModelForm):
-   """
-   新規データ登録画面用のフォーム定義
-   """
-   class Meta:
-       model = Kakeibo
-       fields =['date', 'category', 'money', 'memo']
+    """
+    新規データ登録画面用のフォーム定義
+    """
+    class Meta:
+        model = Kakeibo
+        fields =['date', 'category', 'money', 'memo']
+
+    def clean(self):
+        if not self.is_valid():
+            return self.cleaned_data
+            
+        if not self.cleaned_data["momey"]:
+            raise forms.ValidationError(
+                "金額を入力してください")
+        return self.cleaned_data
--- django/tutorial/kakeibo/models.py
+++ django/tutorial/kakeibo/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 class Category(models.Model):   
     class Meta:
-       #テーブル名の指定
+       #テーブル名
        db_table ="category"
        verbose_name ="カテゴリ"         #追加
        verbose_name_plural ="カテゴリ"
@@ -20,20 +20,32 @@ class Category(models.Model):
 
 
 
+#カテゴリごとに件数を集計する
+class Manager(models.Manager):
+    def sum_by_category(self):
+
+        result = self.values_list("category").annotate(count=models.Count("category"))
+
+        return dict(result)
+
+
+
 
 class Kakeibo(models.Model):
-   class Meta:
+    objects = Manager()
+
+    class Meta:
        #テーブル名
        db_table ="kakeibo"
        verbose_name ="家計簿"         #追加
        verbose_name_plural ="家計簿"
 
-   #カラムの定義
-   date = models.DateField(verbose_name="日付",default=datetime.now)
-   category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
-   money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
-   memo = models.CharField(verbose_name="メモ", max_length=500)
+    #カラムの定義
+    date = models.DateField(verbose_name="日付",default=datetime.now)
+    category = models.ForeignKey(Category, on_delete = models.PROTECT, verbose_name="カテゴリ")
+    money = models.IntegerField(verbose_name="金額", help_text="単位は日本円")
+    memo = models.CharField(verbose_name="メモ", max_length=500)
 
-   def __str__(self):
-       return self.memo
+    def __str__(self):
+        return self.memo
 
