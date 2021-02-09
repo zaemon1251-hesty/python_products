35707d99a714d22dccc152a4668fcd49a9f78148
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-02-09 14:21:10 +0900

    涙の数だけ強くなれるよ

--- django/tutorial/tutorial/settings.py
+++ django/tutorial/tutorial/settings.py
@@ -146,4 +146,4 @@ db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
