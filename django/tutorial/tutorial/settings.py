ed1fa0b5fae80fd580781d0adb9ca69000d44724
zaemon1251-hesty <hisakawa1251@gmail.com>
2021-02-09 12:15:41 +0900

    deactivate whitenoise

--- django/tutorial/tutorial/settings.py
+++ django/tutorial/tutorial/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #追加
+    #'whitenoise.middleware.WhiteNoiseMiddleware', #追加
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
@@ -144,4 +144,4 @@ db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
